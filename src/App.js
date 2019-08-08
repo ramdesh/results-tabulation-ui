8a37c517c950191eea21e9554382e95545d745a1
pawan-manishka <pawan.gunaratne@gmail.com>
2019-08-08 19:08:39 +0530

    Added React content

--- src/App.js
+++ src/App.js
@@ -1,51 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-import theme from './theme';
-
-
-import Header from './components/header'
-
-
-import Button from '@material-ui/core/Button';
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-
-import Pages from './pages'
-
-class App extends Component {
-
-    render() {
-
-        return (
-            <MuiThemeProvider theme={theme}>
-                <Header/>
-
-                <Router>
-                    <div>
-                        {/*<ul>
-                            <li>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li>
-                                <Link to="/about">About</Link>
-                            </li>
-                            <li>
-                                <Link to="/topics">Topics</Link>
-                            </li>
-                        </ul>
-
-                        <hr/>
-*/}
-                        <Route exact path="/" component={Pages.BoxCount}/>
-                        <Route path="/about" component={Pages.IssuingAndReceiving}/>
-                        <Route path="/pre-41" component={Pages.Pre41}/>
-                    </div>
-                </Router>
-            </MuiThemeProvider>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import BoxCount from './box-count'
-import IssuingAndReceiving from './Issuing-and-receiving'
-import Pre41 from './pre-41'
-
-export default {
-    BoxCount,
-    IssuingAndReceiving,
-    Pre41
-}
