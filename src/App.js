ed9731f8be86807881bf959edfc1d6d02f0af8db
pawan-manishka <pawan.gunaratne@gmail.com>
2019-08-10 15:32:49 +0530

    Added Party-wise count UI and changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/navbar/Navbar";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import InvalidBallots from "./components/invalid-ballots/InvalidBallots";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <NavBar/>
+                <Route exact path="/" component={InvalidBallots}/>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
+
+
+
+
+
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import './Navbar.css';
+
+const NavBar = () => {
+    return(
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="title" color='#4879d1'>
+                       Election Result Tabulation
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <div id="main_nav">
+                <ul>
+                    <li>
+                        <a href="">Election Commission</a>
+                        <ul>
+                            <li><a href="">Ratites</a></li>
+                            <li><a href="">Fowl</a></li>
+                            <li><a href="">Neoaves</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">District Result Centre</a>
+                        <ul>
+                            <li>
+                                <a href="">Data Entry</a>
+                                <ul>
+                                    <li><a href="/issuing">Issuing</a></li>
+                                    <li><a href="">Receiving</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Reports</a>
+                                <ul>
+                                    <li><a href="">PRE-30-PD</a></li>
+                                    <li><a href="">PRE-30-PV</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="">Counting Centre</a>
+                        <ul>
+                            <li>
+                                <a href="">Data Entry</a>
+                                <ul>
+                                    <li><a href="">Receiving</a></li>
+                                    <li><a href="">Dispatching</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="">Reports</a>
+                                <ul>
+                                    <li><a href="">PRE-41</a></li>
+                                    <li><a href="">PRE-21</a></li>
+                                    <li><a href="">PRE-30-CO</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default NavBar;
