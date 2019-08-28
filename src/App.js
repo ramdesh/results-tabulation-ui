0ee740bf6f33aaa43c9f98e6d9a5473fcc3f9001
pawan-manishka <pawan.gunaratne@gmail.com>
2019-08-28 15:43:48 +0530

    Added fixes and changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import CE201 from "./components/CE201/CE201";
 import PRE28A from "./components/PRE28A/PRE28A";
 import PRE28AEntry from "./components/PRE28A/PRE28AEntry";
+import Reports from "./components/report/reports";
 import PRE21 from "./components/invalid-ballots/PRE21";
 import PRE21Entry from "./components/invalid-ballots/PRE21Entry";
 import PRE21PVEntry from "./components/invalid-ballots/postal-votes/PRE21PVEntry";
@@ -32,12 +33,14 @@ function App() {
                 <Route exact path="/CE201-Entry" component={CE201Entry}/>
                 <Route exact path="/PRE28A" component={PRE28A}/>
                 <Route exact path="/PRE28A-Entry" component={PRE28AEntry}/>
+                <Route exact path="/report" component={Reports}/>
                 <Route exact path="/PRE21" component={PRE21}/>
                 <Route exact path="/PRE21-Entry" component={PRE21Entry}/>
                 <Route exact path="/PRE21PV" component={PRE21PV}/>
                 <Route exact path="/PRE21PV-Entry" component={PRE21PVEntry}/>
                 <Route exact path="/PRE41" component={PRE41}/>
-                <Route exact path="/PRE41-Entry" component={PRE41Entry}/>
+                <Route path="/PRE41-Entry/:name" component={PRE41Entry} />
+
                 <Route exact path="/PRE41PV" component={PRE41PV}/>
                 <Route exact path="/PRE41PV-Entry" component={PRE41PVEntry}/>
                 <Route exact path="/PRE34CO" component={PRE34CO}/>
