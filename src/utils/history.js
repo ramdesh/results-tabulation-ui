74e0f7c670428d1a4f7838b708f2d8f0bf83b1b1
pawan-manishka <pawan.gunaratne@gmail.com>
2019-10-06 14:42:10 +0530

    Fixed issue

--- src/utils/history.js
+++ src/utils/history.js
@@ -1,4 +1,6 @@
-import { createBrowserHistory } from "src/utils/history";
+import { createBrowserHistory } from "history";
 import { AppConfig } from "../configs";
+
 const appConfig = new AppConfig();
+
 export const history = createBrowserHistory({ basename: appConfig.basePath });
