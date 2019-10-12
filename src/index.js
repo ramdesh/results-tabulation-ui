e52ee64b2a61d8b780d78189204d63c7cd865b0f
Brion Mario <info@brionmario.com>
2019-10-13 01:34:03 +0530

    Wrap the app with `AuthProvider`

--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter} from "react-router-dom";
+import {Router} from "react-router";
+import {AuthProvider} from "./contexts";
+import { history } from "./utils";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <Router history={ history }>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </Router>
+    </BrowserRouter>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
