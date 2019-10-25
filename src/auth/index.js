637b2559dca168d2234bf92f9768cdf7c297de09
Dinuka De Silva <l.dinukadesilva@gmail.com>
2019-10-25 12:13:10 +0530

    Refactoring

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,25 +1,45 @@
 import {Redirect, Route} from "react-router";
 import React, {Component} from "react";
 import {} from "../config";
-import {API_ACCESS_TOKEN_KEY, AUTH_APP_SIGN_IN_URL_PATH} from "./constants";
+import {
+    API_ACCESS_TOKEN_KEY,
+    API_USER_INFO_KEY,
+    API_USER_INFO_USERNAME_KEY,
+    AUTH_APP_SIGN_IN_URL_PATH
+} from "./constants";
 import {AUTH_APP_URL} from "../config";
+import Cookies from 'js-cookie';
 
-
-function getCookie(name) {
-    var value = "; " + document.cookie;
-    var parts = value.split("; " + name + "=");
-    if (parts.length == 2) return parts.pop().split(";").shift();
-}
 
 export function getAuthAppSignInUrl() {
     return `${AUTH_APP_URL}${AUTH_APP_SIGN_IN_URL_PATH}`;
 }
 
 export function getAccessToken() {
-    const tabulationAccessToken = getCookie(API_ACCESS_TOKEN_KEY);
+    const tabulationAccessToken = Cookies.get(API_ACCESS_TOKEN_KEY);
     localStorage.setItem('token', tabulationAccessToken);
 
     return tabulationAccessToken;
+}
+
+export function getUserInfo() {
+    let userInfo = Cookies.get(API_USER_INFO_KEY)
+    if (userInfo !== undefined) {
+        try {
+            userInfo = JSON.parse(userInfo);
+        } catch (e) {
+            userInfo = {};
+        }
+    } else {
+        userInfo = {};
+    }
+
+    return userInfo
+}
+
+export function getUserName() {
+    const userInfo = getUserInfo();
+    return userInfo[API_USER_INFO_USERNAME_KEY]
 }
 
 export function hasValidToken() {
@@ -33,6 +53,17 @@ export function hasValidToken() {
 
 export function redirectToLogin() {
     window.location.href = getAuthAppSignInUrl();
+}
+
+export function logout() {
+
+    //TODO: notify api to revoke access token
+
+    Cookies.remove('userinfo');
+    Cookies.remove('tabulation_access_token');
+
+    localStorage.clear();
+    window.location.reload();
 }
 
 
