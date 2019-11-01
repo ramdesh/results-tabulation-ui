907cb6ae1264de51f5a8837ac5b62b2dad4bcc64
manuranga perera <manu@wso2.com>
2019-11-01 21:09:02 +0530

    duplicate code from report views to create release views

---
+++ src/pages/release-list/index.js
@@ -0,0 +1,279 @@
+import React, {Component, useEffect, useState} from "react";
+import {Link} from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Dialog from '@material-ui/core/Dialog';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItem from '@material-ui/core/ListItem';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+
+import {generateReport, getElections, getTallySheet, TALLY_SHEET_STATUS_ENUM} from "../../services/tabulation-api";
+import {MessagesProvider, MessagesConsumer} from "../../services/messages.provider";
+import {
+    PATH_ELECTION,
+    PATH_ELECTION_BY_ID,
+    PATH_ELECTION_DATA_ENTRY,
+    PATH_ELECTION_DATA_ENTRY_EDIT,
+    PATH_ELECTION_REPORT,
+    PATH_ELECTION_REPORT_VIEW,
+    TALLY_SHEET_CODE_CE_201,
+    TALLY_SHEET_CODE_CE_201_PV,
+    TALLY_SHEET_CODE_PRE_30_ED,
+    TALLY_SHEET_CODE_PRE_30_PD,
+    TALLY_SHEET_CODE_PRE_41,
+    TALLY_SHEET_CODE_PRE_ALL_ISLAND_RESULTS,
+    TALLY_SHEET_CODE_PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS
+} from "../../App";
+import Processing from "../../components/processing";
+import Error from "../../components/error";
+import BreadCrumb from "../../components/bread-crumb";
+import Button from "@material-ui/core/Button";
+import {getTallySheetCodeStr} from "../../utils/tallySheet";
+import TextField from "@material-ui/core/TextField/TextField";
+import {fieldMatch, getFirstOrNull} from "../../utils";
+import {getAreaName} from "../../utils/tallySheet";
+import {VOTE_TYPE} from "../../services/tabulation-api/entities/election.entity";
+
+
+export default function ReportList({history, queryString, election, subElection}) {
+    const {electionId, electionName} = election;
+    const {tallySheetCode} = queryString;
+
+    const [tallySheets, setTallySheets] = useState([]);
+    const [processing, setProcessing] = useState(true);
+    const [error, setError] = useState(false);
+
+    const [searchParameters, setSearchParameters] = React.useState({
+        electoralDistrict: '',
+        pollingDivision: '',
+        countingCentre: '',
+        status: ''
+    });
+
+    const handleChange = name => event => {
+        setSearchParameters({...searchParameters, [name]: event.target.value});
+    };
+
+    function getElection() {
+        return subElection ? subElection : election;
+    }
+
+
+    useEffect(() => {
+        getTallySheet({
+            electionId: getElection().electionId,
+            tallySheetCode,
+            limit: 3000,
+            offset: 0
+        }).then((tallySheets) => {
+            setTallySheets(tallySheets);
+            setProcessing(false);
+        }).catch((error) => {
+            setError(true);
+            setProcessing(false);
+        })
+    }, [])
+
+    function getTallySheetListJsx() {
+        if (processing) {
+            return <Processing/>
+        } else if (error) {
+            return <Error
+                title="Tally sheet list cannot be accessed"
+            />
+        } else {
+            if (tallySheetCode === TALLY_SHEET_CODE_PRE_30_PD) {
+                return getTallySheetListJsx_PRE_30_PD(tallySheets)
+            } else if (tallySheetCode === TALLY_SHEET_CODE_PRE_30_ED) {
+                return getTallySheetListJsx_PRE_30_ED(tallySheets)
+            } else if (tallySheetCode === TALLY_SHEET_CODE_PRE_ALL_ISLAND_RESULTS || TALLY_SHEET_CODE_PRE_ALL_ISLAND_RESULTS_BY_ELECTORAL_DISTRICTS) {
+                return getTallySheetListJsx_AllIslandReports(tallySheets)
+            }
+        }
+    }
+
+    function getActions(tallySheet) {
+        return <TableCell align="center">
+            <Button
+                variant="outlined" color="default"
+                size="small"
+                disabled={!(tallySheet.tallySheetStatus !== TALLY_SHEET_STATUS_ENUM.VERIFIED)}
+
+                onClick={() => history.push(PATH_ELECTION_REPORT_VIEW(electionId, tallySheet.tallySheetId))}
+            >
+                Verify
+            </Button>
+            <Button
+                variant="outlined" color="default"
+                size="small"
+                onClick={() => history.push(PATH_ELECTION_REPORT_VIEW(electionId, tallySheet.tallySheetId))}
+            >
+                View
+            </Button>
+            <Button
+                variant="outlined" color="default"
+                disabled={!(tallySheet.tallySheetStatus === TALLY_SHEET_STATUS_ENUM.VERIFIED)}
+                size="small" disabled={tallySheet.lockedVersionId === null}
+                onClick={() => history.push(PATH_ELECTION_REPORT_VIEW(electionId, tallySheet.tallySheetId))}
+            >
+                Unlock
+            </Button>
+        </TableCell>
+    }
+
+
+    function getTallySheetListJsx_PRE_30_PD(tallySheets) {
+        return <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell align="center" style={{width: "20%"}}>
+                        <TextField
+                            value={searchParameters.electoralDistrict}
+                            margin="dense"
+                            variant="outlined"
+                            placeholder="Search Electoral District"
+                            onChange={handleChange('electoralDistrict')}
+                        />
+                    </TableCell>
+                    <TableCell align="center" style={{width: "20%"}}>
+                        <TextField
+                            value={searchParameters.pollingDivision}
+                            margin="dense"
+                            variant="outlined"
+                            placeholder="Search Polling Division"
+                            onChange={handleChange('pollingDivision')}
+                        />
+                    </TableCell>
+                    <TableCell align="center" style={{width: "10%"}}>
+                        <TextField
+                            value={searchParameters.status}
+                            margin="dense"
+                            variant="outlined"
+                            placeholder="Status"
+                            onChange={handleChange('status')}
+                        />
+                    </TableCell>
+                    <TableCell align="left"></TableCell>
+                    <TableCell align="center"></TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell align="left">Electoral District</TableCell>
+                    <TableCell align="left">Polling Division</TableCell>
+                    <TableCell align="center">Status</TableCell>
+                    <TableCell align="center">Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {tallySheets.map(tallySheet => {
+                    if (fieldMatch(getAreaName(tallySheet.electoralDistrict), searchParameters.electoralDistrict) &&
+                        fieldMatch(tallySheet.tallySheetStatus, searchParameters.status) &&
+                        fieldMatch(getAreaName(tallySheet.pollingDivision), searchParameters.pollingDivision)) {
+                        return <TableRow key={tallySheet.tallySheetId}>
+                            <TableCell align="left">{getAreaName(tallySheet.electoralDistrict)}</TableCell>
+                            <TableCell align="left">{getAreaName(tallySheet.pollingDivision)}</TableCell>
+                            <TableCell align="center">{tallySheet.tallySheetStatus}</TableCell>
+                            {getActions(tallySheet)}
+                        </TableRow>
+                    }
+                })}
+            </TableBody>
+        </Table>
+    }
+
+
+    function getTallySheetListJsx_PRE_30_ED(tallySheets) {
+        return <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell align="center" style={{width: "20%"}}>
+                        <TextField
+                            value={searchParameters.electoralDistrict}
+                            margin="dense"
+                            variant="outlined"
+                            placeholder="Search Electoral District"
+                            onChange={handleChange('electoralDistrict')}
+                        />
+                    </TableCell>
+                    <TableCell align="center" style={{width: "10%"}}>
+                        <TextField
+                            value={searchParameters.status}
+                            margin="dense"
+                            variant="outlined"
+                            placeholder="Status"
+                            onChange={handleChange('status')}
+                        />
+                    </TableCell>
+                    <TableCell align="left"></TableCell>
+                    <TableCell align="center"></TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell align="left">Electoral District</TableCell>
+                    <TableCell align="center">Status</TableCell>
+                    <TableCell align="center">Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {tallySheets.map(tallySheet => {
+                    if (fieldMatch(getAreaName(tallySheet), searchParameters.electoralDistrict) &&
+                        fieldMatch(tallySheet.tallySheetStatus, searchParameters.status)) {
+                        return <TableRow key={tallySheet.tallySheetId}>
+                            <TableCell align="left">{getAreaName(tallySheet.electoralDistrict)}</TableCell>
+                            <TableCell align="center">{tallySheet.tallySheetStatus}</TableCell>
+                            {getActions(tallySheet)}
+                        </TableRow>
+                    }
+                })}
+            </TableBody>
+        </Table>
+    }
+
+    function getTallySheetListJsx_AllIslandReports(tallySheets) {
+        return <Table aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                    <TableCell align="left">Country</TableCell>
+                    <TableCell align="center">Status</TableCell>
+                    <TableCell align="center">Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {tallySheets.map(tallySheet => {
+                    return <TableRow key={tallySheet.tallySheetId}>
+                        <TableCell align="left">{getAreaName(tallySheet.country)}</TableCell>
+                        <TableCell align="center">{tallySheet.tallySheetStatus}</TableCell>
+                        {getActions(tallySheet)}
+                    </TableRow>
+                })}
+            </TableBody>
+        </Table>
+    }
+
+    return <div className="page">
+        <BreadCrumb
+            links={[
+                {label: "elections", to: PATH_ELECTION()},
+                {label: electionName, to: PATH_ELECTION_BY_ID(electionId)},
+                {
+                    label: getTallySheetCodeStr({tallySheetCode, election: getElection()}).toLowerCase(),
+                    to: PATH_ELECTION_REPORT(electionId, tallySheetCode)
+                },
+            ]}
+        />
+        <div className="page-content">
+            <div>{electionName}</div>
+            <div>{getTallySheetCodeStr({tallySheetCode, election: getElection()})}</div>
+            {getTallySheetListJsx()}
+        </div>
+    </div>
+}
+
---
+++ src/pages/release-view.js
@@ -0,0 +1,228 @@
+import React, {Component, useEffect, useState} from "react";
+import {Link} from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import {
+    getElections,
+    getTallySheet,
+    getTallySheetVersionHtml, lockTallySheet, requestEditForTallySheet,
+    saveTallySheetVersion,
+    TALLY_SHEET_STATUS_ENUM, unlockTallySheet
+} from "../services/tabulation-api";
+import {MessagesProvider, MessagesConsumer, MESSAGE_TYPES} from "../services/messages.provider";
+import {
+    PATH_ELECTION, PATH_ELECTION_BY_ID,
+    PATH_ELECTION_DATA_ENTRY, PATH_ELECTION_DATA_ENTRY_EDIT,
+    TALLY_SHEET_CODE_CE_201,
+    TALLY_SHEET_CODE_CE_201_PV,
+    TALLY_SHEET_CODE_PRE_41,
+    TALLY_SHEET_CODE_PRE_34_CO, COUNTING_CENTRE_WISE_DATA_ENTRY_TALLY_SHEET_CODES, PATH_ELECTION_REPORT
+} from "../App";
+import Processing from "../components/processing";
+import Error from "../components/error";
+import BreadCrumb from "../components/bread-crumb";
+import Button from "@material-ui/core/Button";
+import {MESSAGES_EN} from "../locale/messages_en";
+
+
+export default function ReportView(props) {
+    const {history, election, messages} = props
+    const {electionId, electionName} = election;
+    const [tallySheet, setTallySheet] = useState(props.tallySheet);
+    const [tallySheetVersionId, setTallySheetVersionId] = useState(null);
+    const [tallySheetVersionHtml, setTallySheetVersionHtml] = useState("");
+    const [processing, setProcessing] = useState(false);
+    const [error, setError] = useState(false);
+    const [iframeHeight, setIframeHeight] = useState(600);
+    const [iframeWidth, setIframeWidth] = useState("100%");
+    const iframeRef = React.createRef();
+
+
+    const fetchTallySheetVersion = async () => {
+        const {tallySheetId, tallySheetCode, latestVersionId, submittedVersionId, lockedVersionId, tallySheetStatus} = tallySheet;
+        let tallySheetVersionId = null;
+        if (COUNTING_CENTRE_WISE_DATA_ENTRY_TALLY_SHEET_CODES.indexOf(tallySheetCode) >= 0) {
+            if (lockedVersionId) {
+                tallySheetVersionId = lockedVersionId;
+            } else if (submittedVersionId) {
+                tallySheetVersionId = submittedVersionId;
+            } else if (latestVersionId) {
+                tallySheetVersionId = latestVersionId;
+            }
+        } else {
+            if (lockedVersionId) {
+                tallySheetVersionId = lockedVersionId;
+            } else {
+                const tallySheetVersion = await saveTallySheetVersion(tallySheetId, tallySheetCode);
+                tallySheetVersionId = tallySheetVersion.tallySheetVersionId;
+            }
+        }
+
+        setTallySheetVersionId(tallySheetVersionId);
+    };
+
+    const fetchTallySheetVersionHtml = async () => {
+        setTallySheetVersionHtml("Processing ... ");
+        const {tallySheetId} = tallySheet;
+        const tallySheetVersionHtml = await getTallySheetVersionHtml(tallySheetId, tallySheetVersionId);
+
+        setTallySheetVersionHtml(tallySheetVersionHtml)
+    };
+
+    useEffect(() => {
+        fetchTallySheetVersion();
+    }, [tallySheet]);
+
+    useEffect(() => {
+        tallySheetVersionId && fetchTallySheetVersionHtml();
+    }, [tallySheetVersionId]);
+
+    const handleIframeHeight = () => (evt) => {
+        setIframeHeight(evt.target.contentDocument.documentElement.scrollHeight + 50);
+    };
+
+    const handlePrint = () => (evt) => {
+        iframeRef.current.contentWindow.print();
+    }
+
+    const handleRequestEdit = () => async (evt) => {
+        setProcessing(true);
+        const {tallySheetId} = tallySheet;
+        try {
+            const tallySheet = await requestEditForTallySheet(tallySheetId);
+            setTallySheet(tallySheet);
+            messages.push("Success", MESSAGES_EN.success_report_editable, MESSAGE_TYPES.SUCCESS);
+            setTimeout(() => {
+                history.push(PATH_ELECTION_DATA_ENTRY_EDIT(electionId, tallySheetId))
+            }, 1000)
+        } catch (e) {
+            messages.push("Error", MESSAGES_EN.error_updating_report, MESSAGE_TYPES.ERROR);
+        }
+        setProcessing(false);
+    };
+
+    const handleVerify = () => async (evt) => {
+        setProcessing(true);
+        const {tallySheetId} = tallySheet;
+        try {
+            const tallySheet = await lockTallySheet(tallySheetId, tallySheetVersionId);
+            setTallySheet(tallySheet);
+            messages.push("Success", MESSAGES_EN.success_report_verify, MESSAGE_TYPES.SUCCESS);
+        } catch (e) {
+            messages.push("Error", MESSAGES_EN.error_verifying_report, MESSAGE_TYPES.ERROR);
+        }
+        setProcessing(false);
+    };
+
+    const handleUnlock = () => async (evt) => {
+        setProcessing(true);
+        const {tallySheetId} = tallySheet;
+        try {
+            const tallySheet = await unlockTallySheet(tallySheetId);
+            await setTallySheet(tallySheet);
+            messages.push("Success", MESSAGES_EN.success_report_unlock, MESSAGE_TYPES.SUCCESS);
+            //fetchTallySheetVersion();
+        } catch (e) {
+            messages.push("Error", MESSAGES_EN.error_unlock_report, MESSAGE_TYPES.ERROR);
+        }
+        setProcessing(false);
+    };
+
+    const getReportViewJsx = () => {
+        const {tallySheetCode, tallySheetStatus} = tallySheet;
+        const subElectionId = tallySheet.electionId;
+
+        const breadCrumbLinkList = [
+            {label: "elections", to: PATH_ELECTION()},
+            {label: electionName, to: PATH_ELECTION_BY_ID(electionId)}
+        ];
+        if (COUNTING_CENTRE_WISE_DATA_ENTRY_TALLY_SHEET_CODES.indexOf(tallySheetCode) >= 0) {
+            breadCrumbLinkList.push({
+                label: tallySheetCode.toLowerCase(),
+                to: PATH_ELECTION_DATA_ENTRY(electionId, tallySheetCode, subElectionId)
+            })
+        } else {
+            breadCrumbLinkList.push({
+                label: tallySheetCode.toLowerCase(),
+                to: PATH_ELECTION_REPORT(electionId, tallySheetCode, subElectionId)
+            })
+        }
+
+        return <div className="page">
+            <BreadCrumb
+                links={breadCrumbLinkList}
+            />
+            <div className="page-content">
+                <div>{electionName}</div>
+                <div>{tallySheetCode}</div>
+
+
+                <div className="report-view-status">
+                    <div className="report-view-status-actions">
+                        <Button variant="contained" size="small" color="default" onClick={handlePrint()}>
+                            Print
+                        </Button>
+                        <Button
+                            variant="contained" size="small" color="primary"
+                            disabled={processing || !tallySheet.readyToLock}
+                            onClick={handleVerify()}
+                        >
+                            Confirm
+                        </Button>
+                        {(() => {
+                            console.log("TALLY_SHEET_CODE_PRE_41", `${tallySheetCode}-`);
+                            if (tallySheetCode === TALLY_SHEET_CODE_PRE_41 || tallySheetCode === TALLY_SHEET_CODE_CE_201 || tallySheetCode === TALLY_SHEET_CODE_CE_201_PV) {
+                                return <Button
+                                    variant="contained" size="small" color="primary"
+                                    disabled={processing || !tallySheet.readyToLock}
+                                    onClick={handleRequestEdit()}
+                                >
+                                    Edit
+                                </Button>
+                            }
+                        })()}
+                        <Button
+                            variant="contained" size="small" color="primary"
+                            disabled={!(tallySheetStatus === TALLY_SHEET_STATUS_ENUM.VERIFIED)}
+                            onClick={handleUnlock()}
+                        >
+                            Unlock
+                        </Button>
+                    </div>
+                    <div className="report-view-status-text">
+                        {(() => {
+                            if (tallySheetStatus == TALLY_SHEET_STATUS_ENUM.SUBMITTED) {
+                                return "This report has been submitted to the system and waiting for verification";
+                            } else if (tallySheetStatus == TALLY_SHEET_STATUS_ENUM.VIEWED) {
+                                return "This report has been not verified yet";
+                            } else if (tallySheetStatus == TALLY_SHEET_STATUS_ENUM.ENTERED) {
+                                return "This report has no submitted or verified information. The editing is still in progress.";
+                            } else if (tallySheetStatus == TALLY_SHEET_STATUS_ENUM.VERIFIED) {
+                                return "This report has been verified.";
+                            } else if (tallySheetStatus == TALLY_SHEET_STATUS_ENUM.RELEASED) {
+                                return "This report has been released.";
+                            }
+                        })()}
+                    </div>
+                </div>
+
+
+                <iframe
+                    style={{border: "none", width: "100%"}}
+                    height={iframeHeight}
+                    width={iframeWidth}
+                    srcDoc={tallySheetVersionHtml}
+                    onLoad={handleIframeHeight()}
+                    ref={iframeRef}
+                >
+                </iframe>
+            </div>
+        </div>
+    }
+
+    return getReportViewJsx()
+}
